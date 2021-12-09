e411bb21f70c7fb7aa15123f1ce7b4322d8d4d45
Mariano Sousa <mariano.sousa94@gmail.com>
2021-12-09 17:40:15 -0100

    output in collumns

--- detect-upgrades.js
+++ detect-upgrades.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const https = require("https");
+const columns = require("cli-columns");
 
 const dir = "./projects/";
 
@@ -14,77 +15,100 @@ if (args.length == 0) {
   return;
 }
 
-// for each project
-args.forEach((arg) => {
-  argSplit = arg.split(":");
-  const dependency = {
-    repository: argSplit[0],
-    currentVersion: argSplit[1],
-  };
+// for each dependency
+var output = ["Respository", "Latest Major", "Latest Minor", "Latest Patch"];
+console.log(doColumns(output));
+// args.forEach(async (arg) => {});
+processDependencies(args);
+// console.log(columns(output, { sort: false }));
 
-  detectDependencyNewVersions(dependency);
-});
+async function processDependencies(args) {
+  for (let i = 0; i < args.length; i++) {
+    argSplit = args[i].split(":");
+    const dependency = {
+      repository: argSplit[0],
+      currentVersion: argSplit[1],
+    };
+
+    const result = await detectDependencyNewVersions(dependency);
+    output = [
+      dependency.repository,
+      result.latestMajor,
+      result.latestMinor,
+      result.latestPatch,
+    ];
+
+    console.log(doColumns(output));
+  }
+}
+
+function doColumns(output) {
+  return columns(output, { sort: false, character: " ", padding: 5 });
+}
 
 function detectDependencyNewVersions(dependency) {
-  const options = {
-    host: "registry.hub.docker.com",
-    path: `/v2/repositories/${dependency.repository}/tags/?page_size=9999999`,
-  };
+  return new Promise((resolve, reject) => {
+    const options = {
+      host: "registry.hub.docker.com",
+      path: `/v2/repositories/${dependency.repository}/tags/?page_size=9999999`,
+    };
 
-  https.get(options, (res) => {
-    res.setEncoding("utf-8");
+    https.get(options, (res) => {
+      res.setEncoding("utf-8");
 
-    var data = "";
+      var data = "";
 
-    res.on("data", function (chunk) {
-      data += chunk;
-    });
+      res.on("data", function (chunk) {
+        data += chunk;
+      });
 
-    res.on("end", () => {
-      // console.log(dependency);
-      console.log(
-        `Current ${dependency.repository} Version: ${dependency.currentVersion}`
-      );
-      const [currentMajor, currentMinor, currentPatch] =
-        dependency.currentVersion.split(".");
-      // console.log(data);
-      var latestTags = JSON.parse(data);
-      var versionsArray = [];
-      latestTags.results.forEach((tag) => {
-        let [tagMajor, tagMinor, tagPatch] = tag.name.split(".");
-        versionsArray.push({
-          major: Number.parseInt(tagMajor),
-          minor: Number.parseInt(tagMinor),
-          patch: Number.parseInt(tagPatch),
-          updated: tag.last_updated,
+      res.on("end", () => {
+        const [currentMajor, currentMinor, currentPatch] =
+          dependency.currentVersion.split(".");
+        var latestTags = JSON.parse(data);
+        var versionsArray = [];
+        latestTags.results.forEach((tag) => {
+          let [tagMajor, tagMinor, tagPatch] = tag.name.split(".");
+          versionsArray.push({
+            major: Number.parseInt(tagMajor),
+            minor: Number.parseInt(tagMinor),
+            patch: Number.parseInt(tagPatch),
+            updated: tag.last_updated,
+          });
         });
-      });
-      var latestMajor = 0;
-      var latestMinor = 0;
-      var latestPatch = 0;
-      versionsArray.forEach((version) => {
-        if (version.major > latestMajor)
-          latestMajor = Number.parseInt(version.major);
-        if (
-          Number.parseInt(currentMajor) == Number.parseInt(version.major) &&
-          version.minor > latestMinor
-        )
-          latestMinor = version.minor;
-        if (
-          Number.parseInt(currentMajor) == Number.parseInt(version.major) &&
-          Number.parseInt(currentMinor) == Number.parseInt(version.minor) &&
-          version.patch > latestPatch
-        )
-          latestPatch = version.patch;
-      });
+        var latestMajor = 0;
+        var latestMinor = 0;
+        var latestPatch = 0;
+        versionsArray.forEach((version) => {
+          if (version.major > latestMajor)
+            latestMajor = Number.parseInt(version.major);
+          if (
+            Number.parseInt(currentMajor) == Number.parseInt(version.major) &&
+            version.minor > latestMinor
+          )
+            latestMinor = version.minor;
+          if (
+            Number.parseInt(currentMajor) == Number.parseInt(version.major) &&
+            Number.parseInt(currentMinor) == Number.parseInt(version.minor) &&
+            version.patch > latestPatch
+          )
+            latestPatch = version.patch;
+        });
 
-      const result = {
-        latestMajor: latestMajor + "",
-        latestMinor: currentMajor + "." + latestMinor,
-        latestPatch: currentMajor + "." + currentMinor + "." + latestPatch,
-      };
+        const result = {
+          latestMajor: currentMajor != latestMajor ? latestMajor + "" : "-",
+          latestMinor:
+            currentMinor != latestMinor
+              ? currentMajor + "." + latestMinor
+              : "-",
+          latestPatch:
+            currentPatch != latestPatch
+              ? currentMajor + "." + currentMinor + "." + latestPatch
+              : "-",
+        };
 
-      console.table(result);
+        resolve(result);
+      });
     });
   });
 }
